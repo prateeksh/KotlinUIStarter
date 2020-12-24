b79aeb1caa2d6a5bf724b5398b8d1295514195fd
Prateek Sharma <sharma.prateek95@gmail.com>
2020-12-24 21:01:13 +0530

    updated ui

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -1,8 +1,11 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
+import android.os.Build
 import android.os.Bundle
 import android.view.*
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,6 +21,13 @@ class ProductGridFragment : Fragment() {
         val view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
 
         (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
+        view.app_bar.setNavigationOnClickListener(NavigationIconClickListener(
+                activity!!,
+                view.product_grid,
+                AccelerateDecelerateInterpolator(),
+                ContextCompat.getDrawable(context!!, R.drawable.shr_branded_menu),
+                ContextCompat.getDrawable(context!!, R.drawable.shr_close_menu)
+        ))
         view.recycler_view.setHasFixedSize(true)
         val gridLayoutManager = GridLayoutManager(context, 2, RecyclerView.HORIZONTAL, false)
         gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup(){
@@ -33,6 +43,9 @@ class ProductGridFragment : Fragment() {
         val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
         view.recycler_view.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
 
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            view.product_grid.background = context?.getDrawable(R.drawable.shr_product_grid_background_shape)
+        }
         return view
     }
 
